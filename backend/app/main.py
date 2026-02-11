a69f1e6fd6668ce444500d6e35ffd125e892bc96
Eric Bueno Corrêa Brida <ericbrida2@gmail.com>
2026-02-11 20:58:49 -0300

    🔧 chore: Inicio da conexão do Banco de dados

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, EmailStr
 from typing import List, Annotated
+from app import models
+from app.database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
